1aa292e0fe349d191298e5ccf562f14334d4945b
Gokul Raja <gokulraja840@gmail.com>
2025-07-03 10:12:37 +0530

    update

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,29 +1,25 @@
-<<<<<<< HEAD
-=======
+
 import { string } from 'prop-types';
 
->>>>>>> feature-1
+
 import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-<<<<<<< HEAD
       <h2>{title}</h2>
-=======
+
       <h3>{title}</h3>
->>>>>>> feature-1
+
       <p>{text}</p>
     </article>
   );
 }
 
-<<<<<<< HEAD
-=======
 HelpBox.propTypes = {
   title: string,
   text: string,
 };
 
->>>>>>> feature-1
+
 export default HelpBox;
